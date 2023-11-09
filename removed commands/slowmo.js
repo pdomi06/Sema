7c936feadf4093a44c74af7f92d5a0b9d7725d92
pdomi06 <16976765+pdomi06@users.noreply.github.com>
2023-11-09 22:27:11 +0100

    Should not have unused variables JS-0128
    Found variables that are declared but not used anywhere.

--- removed commands/slowmo.js
+++ removed commands/slowmo.js
@@ -1,50 +1,51 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js'); 
-const { client } = require("../index.js"); 
-require('dotenv').config() 
- 
- 
-module.exports = { 
-  data: new SlashCommandBuilder() 
-    .setName('slowmo') 
-    .setDescription('Applies slowmo to the music. (toggle)'), 
- 
-  async execute(interaction) { 
-    const queue = client.distube.getQueue(interaction) 
- 
-    if (!queue) { 
-      const noQueueEmbed = new EmbedBuilder() 
-        .setColor(process.env.RED) 
-        .setTitle("❌ | There is nothing in the queue") 
-        .setTimestamp() 
-        .setFooter({ text: ' ' }); 
-         
-      await interaction.reply({ embeds: [noQueueEmbed], ephemeral: true }) 
-    } 
- 
-    const filter = "vaporwave" 
-     
- if (Object.keys(client.distube.filters).includes(filter)) { 
-      if (queue.filters.has(filter)){ queue.filters.remove(filter) 
-      let title = "✅ | Slowmo removed"  
-      } else { 
-        queue.filters.add(filter) 
-         title = "✅ | Slowmo added:" 
-      } 
-    } 
- 
- 
-    const slowmoEmbed = new EmbedBuilder() 
-    .setColor(process.env.GREEN) 
-    .setTitle(title) 
-    .setTimestamp() 
-    .setFooter({ text: ' ' }); 
-     
-  await interaction.reply({ embeds: [slowmoEmbed], ephemeral: true }) 
- 
-  setTimeout(() => { 
-    interaction.deleteReply(); 
-  }, 5000); 
-  }, 
- 
-}; 
- 
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { client } = require("../index.js");
+require('dotenv').config()
+
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('slowmo')
+    .setDescription('Applies slowmo to the music. (toggle)'),
+
+  async execute(interaction) {
+    const queue = client.distube.getQueue(interaction)
+
+    if (!queue) {
+      const noQueueEmbed = new EmbedBuilder()
+        .setColor(process.env.RED)
+        .setTitle("❌ | There is nothing in the queue")
+        .setTimestamp()
+        .setFooter({ text: ' ' });
+
+      await interaction.reply({ embeds: [noQueueEmbed], ephemeral: true })
+    }
+
+    const filter = "vaporwave"
+    let title;
+    if (Object.keys(client.distube.filters).includes(filter)) {
+      if (queue.filters.has(filter)) {
+        queue.filters.remove(filter)
+        title = "✅ | Slowmo removed"
+      } else {
+        queue.filters.add(filter)
+        title = "✅ | Slowmo added:"
+      }
+    }
+
+
+    const slowmoEmbed = new EmbedBuilder()
+      .setColor(process.env.GREEN)
+      .setTitle(title)
+      .setTimestamp()
+      .setFooter({ text: ' ' });
+
+    await interaction.reply({ embeds: [slowmoEmbed], ephemeral: true })
+
+    setTimeout(() => {
+      interaction.deleteReply();
+    }, 5000);
+  },
+
+};
+
