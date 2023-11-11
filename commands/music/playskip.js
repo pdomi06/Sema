8aa694440c51bef9eba7812e3844a593048c6da9
pdomi06 <16976765+pdomi06@users.noreply.github.com>
2023-11-12 00:54:23 +0100

    JS-0240
    Use shorthand property syntax for object literals

--- commands/music/playskip.js
+++ commands/music/playskip.js
@@ -1,45 +1,48 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js'); 
-const { client } = require("../../index.js"); 
-require('dotenv').config() 
- 
- 
-module.exports = { 
-  data: new SlashCommandBuilder() 
-    .setName('playskip') 
-    .setDescription('Adds a new song to the queue and skip one.') 
-    .addStringOption(option => option.setName('music') 
-      .setDescription('The URL or title of the song to play') 
-      .setRequired(true)), 
- 
-      async execute(interaction) { 
-        const song = interaction.options.get('music').value; 
-        const member = interaction.member; 
-        const voiceChannel = member.voice.channel; 
-       
-        if (!voiceChannel) { 
-          return interaction.reply({ content: "You must be in a voice channel to use this command.", ephemeral: true }); 
-        } 
-       
-        const queue = client.distube.getQueue(interaction.guildId); 
-       
-        try { 
-       
-            await client.distube.play(voiceChannel, song, { textChannel: interaction.channel, member: member }); 
-            await queue.skip() 
- 
-       
-            const playEmbed = new EmbedBuilder() 
-            .setColor(process.env.GREEN) 
-            .setTitle(" ✅ | Preparing song: ") 
-            .setDescription(song) 
-            .setTimestamp() 
-            .setFooter({ text: ' ' }); 
- 
-          return interaction.reply({ embeds: [playEmbed] }); 
-        } catch (error) { 
-          console.error(error); 
-          return interaction.reply({ content: '❌ Error executing command.', ephemeral: true }); 
-        } 
-      } 
-       
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { client } = require("../../index.js");
+require('dotenv').config()
+
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('playskip')
+    .setDescription('Adds a new song to the queue and skip one.')
+    .addStringOption(option => option.setName('music')
+      .setDescription('The URL or title of the song to play')
+      .setRequired(true)),
+
+  async execute(interaction) {
+    const song = interaction.options.get('music').value;
+    const member = interaction.member;
+    const voiceChannel = member.voice.channel;
+
+    if (!voiceChannel) {
+      return interaction.reply({ content: "You must be in a voice channel to use this command.", ephemeral: true });
+    }
+
+    const queue = client.distube.getQueue(interaction.guildId);
+
+    try {
+
+      await client.distube.play(voiceChannel, song, { 
+        textChannel: interaction.channel,
+        member: member 
+        });
+      await queue.skip()
+
+
+      const playEmbed = new EmbedBuilder()
+        .setColor(process.env.GREEN)
+        .setTitle(" ✅ | Preparing song: ")
+        .setDescription(song)
+        .setTimestamp()
+        .setFooter({ text: ' ' });
+
+      return interaction.reply({ embeds: [playEmbed] });
+    } catch (error) {
+      console.error(error);
+      return interaction.reply({ content: '❌ Error executing command.', ephemeral: true });
+    }
+  }
+
 };
