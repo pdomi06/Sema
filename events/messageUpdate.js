acd4ee4a7a756aa48553e5417fb0b28c44519b12
pdomi06 <16976765+pdomi06@users.noreply.github.com>
2023-11-13 21:48:42 +0100

    Added event on message edit

---
+++ events/messageUpdate.js
@@ -0,0 +1,28 @@
+const { Events, EmbedBuilder } = require("discord.js");
+require("dotenv").config();
+
+module.exports = {
+  name: Events.MessageUpdate,
+  async execute(oldMessage, newMessage) {
+    const db = new QuickDB();
+
+    const id = await db.get(`${message.guild.id}.log_id`);
+    const ch = message.client.channels.cache.get(id);
+    const messageUpdateEmbed = new EmbedBuilder()
+      .setColor(process.env.YELLOW)
+      .setTitle("Message edited")
+      .addFields(
+        { name: "Channel: ", value: `<#${newMessage.channelId}>`, inline: true },
+        {
+          name: "Msg Author: ",
+          value: `<@${newMessage.author.id}>`,
+          inline: true,
+        },
+        { name: "Original: ", value: `\`${oldMessage.content}\`` },
+        { name: "New: ", value: `\`${newMessage.content}\`` },
+      )
+      .setTimestamp()
+      .setFooter({ text: " " });
+    ch?.send({ embeds: [messageUpdateEmbed] });
+  },
+};
