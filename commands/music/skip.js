f66b071b058f344407a28d52d247b071806b184f
pdomi06 <16976765+pdomi06@users.noreply.github.com>
2024-08-24 03:40:35 +0200

    fix

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -1,46 +1,44 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const { client } = require("../../index.js");
-require("dotenv").config();
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js'); 
+const { client } = require("../../index.js"); 
+require('dotenv').config() 
+ 
+module.exports = { 
+  data: new SlashCommandBuilder() 
+    .setName('skip') 
+    .setDescription('Skips one in the queue'), 
+ 
+      async execute(interaction) { 
 
-<<<<<<< HEAD
         const voiceChannel = interaction.member;
         const queue = client.distube.getQueue(interaction) 
-=======
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("skip")
-    .setDescription("Skips one in the queue"),
->>>>>>> 01bf2485628d069242aa50e1dbb072a1b6c81d44
 
-  async execute(interaction) {
-    const voiceChannel = member.voice.channel;
-    const queue = client.distube.getQueue(interaction);
+        if (!voiceChannel) {
+          return interaction.reply({
+            content: "You must be in a voice channel to use this command.",
+            ephemeral: true,
+          });
+        }
 
-    if (!voiceChannel) {
-      return interaction.reply({
-        content: "You must be in a voice channel to use this command.",
-        ephemeral: true,
-      });
-    }
-
-    if (!queue) {
-      const noQueueEmbed = new EmbedBuilder()
-        .setColor(process.env.RED)
-        .setTitle("❌ | There is nothing in the queue")
-        .setTimestamp()
-        .setFooter({ text: " " });
-
-      return interaction.reply({ embeds: [noQueueEmbed], ephemeral: true });
-    }
-    try {
-      await queue.skip();
-      await interaction.reply({ content: "Skipped the song", ephemeral: true });
-    } catch (e) {
-      interaction.reply(` ${e}`);
-    }
-
-    setTimeout(() => {
-      interaction.deleteReply();
-    }, 5000);
-  },
-};
+        if (!queue) { 
+          const noQueueEmbed = new EmbedBuilder() 
+            .setColor(process.env.RED) 
+            .setTitle("❌ | There is nothing in the queue") 
+            .setTimestamp() 
+            .setFooter({ text: ' ' }); 
+             
+          return interaction.reply({ embeds: [noQueueEmbed], ephemeral: true }) 
+        } 
+        try { 
+          await queue.skip() 
+          await interaction.reply({content: "Skipped the song", ephemeral: true }) 
+ 
+        } catch (e) { 
+          interaction.reply(` ${e}`) 
+        } 
+ 
+        setTimeout(() => { 
+          interaction.deleteReply(); 
+        }, 5000); 
+      } 
+       
+}; 
